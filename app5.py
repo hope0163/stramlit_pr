81dee6e5e8290545647151e4431f030edb001cb7
hope0163 <pupple2327@naver.com>
2024-05-20 15:06:36 +0900

    Update app5.py

--- app5.py
+++ app5.py
@@ -18,7 +18,7 @@ def main():
 
     # 동영상 파일을 화면에 표시
     video_file = open('./data/video1.mp4', 'rb') # 비디오파일 불러오기
-    st.video(video_file)
+    st.video(video_file, loop=True, start_time=True)
 
     # 오디오 파일
     audio_file = open('./data/song.mp3', 'rb')
